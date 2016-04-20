a28025acf915438a9d7c6bbc2f1b63574f1d1fe3
Toralf Förster <toralf.foerster@gmx.de>
2016-04-20 17:08:37 +0200

    ps.py: print differences too
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- ps.py
+++ ps.py
@@ -12,43 +12,43 @@ from stem.util.connection import get_connections, system_resolvers, port_usage
 
 def main():
   with Controller.from_port(port = 9051) as controller:
-    controller.authenticate()
 
-    relays = {}    # store here the tupel <ip address, ORport>
+    def printOut (cur, old):
+      "prints the current values and the diff"
+      os.system('clear')
+      print ("   port  count    +/- %s" % time.strftime("%c"))
+      for port in sorted(cur.keys()):
+        count = cur[port]
+        if old[port]:
+          diff = count - old[port]
+        else:
+          diff = count
+        print ("  %5i  %5i %6i (%s)" % (port, count, diff, port_usage(port)))
+      return
+
+    controller.authenticate()
+    relays = {}                                 # store here the tupel <ip address, ORport>
     for s in controller.get_network_statuses():
       relays.setdefault(s.address, []).append(s.or_port)
-
-    class Port(Counter):
-      def __str__(self):
-        ret = ""
-        for port in sorted(self.keys()):
-          n = self[port]
-          ret += "  %5i  %4i (%s)\n" % (port, n, port_usage(port))
-        return ret.rstrip("\n")
-
     policy = controller.get_exit_policy()
 
+    Cur = Counter()
     while True:
       try:
         connections = get_connections('lsof', process_name='tor')
 
-        ports = Port()
+        Old = Cur
+        Cur = Counter()
         for conn in connections:
           if conn.protocol == 'udp':
             continue
-
           raddr = conn.remote_address
           if raddr in relays:
             continue
-
           rport = conn.remote_port
           if policy.can_exit_to(raddr, rport):
-            ports[rport] += 1;
-
-        os.system('clear')
-        print ("   port count")
-        print (ports)
-        time.sleep(1)
+            Cur[rport] += 1;
+        printOut (Cur, Old)
 
       except KeyboardInterrupt:
         break
