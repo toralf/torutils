c6b1a7549b840e87e0355414151548335d8169a5
Toralf Förster <toralf.foerster@gmx.de>
2016-04-21 17:08:20 +0200

    ps.py: test for the relay ORPort too
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- ps.py
+++ ps.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# exit port stats of a running Tor relay
+# exit port stats of a running Tor relay, eg.:
 #
-#eg.:
-#
-   #port   cur   old opened closed   Thu Apr 21 15:56:52 2016
-     #81     4     4      0      0   (HTTP Alternate)
-     #88     1     1      0      0   (Kerberos)
-    #443   491   490     13     12   (HTTPS)
-    #993     6     6      0      0   (IMAPS)
-   #1500     2     2      0      0   (NetGuard)
-   #3128     1     1      0      0   (SQUID)
-   #3389     1     1      0      0   (WBT)
-   #5222    21    21      0      0   (Jabber)
-   #5228    10    10      0      0   (Android Market)
-   #6667     4     4      0      0   (IRC)
-   #8082     4     4      0      0   (None)
-   #8333     4     4      0      0   (Bitcoin)
-   #8888     1     1      0      0   (NewsEDGE)
-   #9999     5     5      0      0   (distinct)
-  #50002    15    15      0      0   (Electrum Bitcoin SSL)
+#   port   cur   old opened closed   8.5 sec
+#     81     4     4      0      0   (HTTP Alternate)
+#     88     1     1      0      0   (Kerberos)
+#    443   491   490     13     12   (HTTPS)
+#    993     6     6      0      0   (IMAPS)
+#   1500     2     2      0      0   (NetGuard)
+#   3128     1     1      0      0   (SQUID)
+#   3389     1     1      0      0   (WBT)
+#   5222    21    21      0      0   (Jabber)
+#   5228    10    10      0      0   (Android Market)
+#   6667     4     4      0      0   (IRC)
+#   8082     4     4      0      0   (None)
+#   8333     4     4      0      0   (Bitcoin)
+#   8888     1     1      0      0   (NewsEDGE)
+#   9999     5     5      0      0   (distinct)
+#  50002    15    15      0      0   (Electrum Bitcoin SSL)
 
 import os
 import time
-from collections import Counter
 from stem.control import Controller
-from stem.util.connection import get_connections, system_resolvers, port_usage
+from stem.util.connection import get_connections, port_usage
 
 def main():
   with Controller.from_port(port = 9051) as controller:
 
-    def printOut (cur, old):
+    def printOut (cur, old, t):
       os.system('clear')
-      print ("   port   cur   old opened closed   %s" % time.strftime("%c"))
+      print ("   port   cur   old opened closed   %.1f sec" % t)
       for port in sorted(cur.keys()):
         n = set(cur[port])
         if port in old:
@@ -47,32 +44,34 @@ def main():
       return
 
     controller.authenticate()
-
-    relays = {}                                 # store here the tupel <ip address, ORport>
+    policy = controller.get_exit_policy()
+    relays = {}                                 # store here the tupel <ip address, ORPort>
     for s in controller.get_network_statuses():
       relays.setdefault(s.address, []).append(s.or_port)
 
-    policy = controller.get_exit_policy()
-
     Cur = {}
+    time2 = time.time()
+
     while True:
       try:
+        time1 = time2
         connections = get_connections('lsof', process_name='tor')
 
         Old = Cur.copy()
-        Cur.clear()
+        Cur = {}
         for conn in connections:
           if conn.protocol == 'udp':
             continue
-          raddr = conn.remote_address
+          raddr, rport = conn.remote_address, conn.remote_port
           if raddr in relays:
-            continue
-          rport = conn.remote_port
+            #continue
+            if rport in relays[raddr]:
+              continue
           if policy.can_exit_to(raddr, rport):
             Cur.setdefault(rport, []).append(raddr)
-            #Cur.setdefault(rport, {}).add(raddr)
 
-        printOut (Cur, Old)
+        time2 = time.time()
+        printOut (Cur, Old, time2 - time1)
 
       except KeyboardInterrupt:
         break
