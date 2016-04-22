ebfd8764d3d49ec4a54a04464510baf6c72fc7cf
Toralf Förster <toralf.foerster@gmx.de>
2016-04-22 10:55:43 +0200

    ps.py: simplify time measurement logic
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- ps.py
+++ ps.py
@@ -3,7 +3,7 @@
 
 # exit port stats of a running Tor relay, eg.:
 #
-#   port   cur   old opened closed   8.5 sec
+#   port   curr   prev opened closed   8.5 sec
 #     81     4     4      0      0   (HTTP Alternate)
 #     88     1     1      0      0   (Kerberos)
 #    443   491   490     13     12   (HTTPS)
@@ -28,13 +28,13 @@ from stem.util.connection import get_connections, port_usage
 def main():
   with Controller.from_port(port = 9051) as controller:
 
-    def printOut (cur, old, t):
+    def printOut (curr, prev, duration):
       os.system('clear')
-      print ("   port   cur   old opened closed   %.1f sec" % t)
-      for port in sorted(cur.keys()):
-        n = set(cur[port])
-        if port in old:
-          o = set(old[port])
+      print ("   port   curr prev opened closed   %.1f sec" % duration)
+      for port in sorted(curr.keys()):
+        n = set(curr[port])
+        if port in prev:
+          o = set(prev[port])
         else:
           o = set({})
         opened = n - o
@@ -45,26 +45,24 @@ def main():
 
     controller.authenticate()
 
-    Cur = {}
-    time2 = time.time()
+    Curr = {}
 
     while True:
       try:
-        time1 = time2
+        start_time = time.time()
 
         connections = get_connections('lsof', process_name='tor')
         policy = controller.get_exit_policy()
 
-        Old = Cur.copy()
-        Cur.clear()
+        Prev = Curr.copy()
+        Curr.clear()
 
         for conn in connections:
           raddr, rport, lport = conn.remote_address, conn.remote_port, conn.local_port
           if policy.can_exit_to(raddr, rport):
-            Cur.setdefault(rport, []).append(str(lport) + ':' + raddr)
+            Curr.setdefault(rport, []).append(str(lport) + ':' + raddr)
 
-        time2 = time.time()
-        printOut (Cur, Old, time2 - time1)
+        printOut (Curr, Prev, time.time() - start_time)
 
       except KeyboardInterrupt:
         break
