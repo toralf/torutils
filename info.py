8ec5804a157adec2a926daf1eed4655180da229a
Toralf Förster <toralf.foerster@gmx.de>
2016-04-28 18:42:18 +0200

    info.py: add example output as a comment
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- info.py
+++ info.py
@@ -3,6 +3,42 @@
 
 # country based stats of a Tor relay
 #
+# eg.:
+#
+# 0.2.8.2-alpha   1 day, 6:04:51   Exit  Fast  Guard  Running  Stable  V2Dir  Valid
+# overall            5664   IPv4: 5663   IPv6:    1
+# CtrlPort <= local     1   ??  1
+# DirPort  <= outer     1   hu  1
+# ORPort   <= outer    63   us 14    de 14    fr  8    nl  5    at  3    ua  2    hk  2    gb  2
+# ORPort   <= relay  3220   de684    us582    fr522    nl266    gb155    ru152    ca125    se123
+# => relay ORPort      34   fr 10    de  7    gb  3    ca  2    us  2    se  2    fi  2    au  1
+# => relay port         4   us  2    gb  1    de  1
+#            :  443     2 (HTTPS)
+#            : 5222     1 (Jabber)
+#            :50002     1 (Electrum Bitcoin SSL)
+# => non exit port      1   ch  1
+#            : 9001     1 (None)
+# => exit w/o www     173   us 68    de 24    nl 15    ru 14    fr  8    ca  7    gb  5    jp  4
+#            :   81     2 (HTTP Alternate)
+#            :  443  2165 (HTTPS)
+#            :  992     3 (Telnets)
+#            :  993    14 (IMAPS)
+#            :  995     1 (POP3S)
+#            : 1500     7 (NetGuard)
+#            : 3128     2 (SQUID)
+#            : 3389     1 (WBT)
+#            : 5050     1 (Yahoo IM)
+#            : 5190     2 (AIM/ICQ)
+#            : 5222    65 (Jabber)
+#            : 5228    30 (Android Market)
+#            : 6664     1 (IRC)
+#            : 6667     4 (IRC)
+#            : 8000     1 (iRDMI)
+#            : 8082     1 (None)
+#            : 8333    15 (Bitcoin)
+#            : 8443     5 (PCsync HTTPS)
+#            : 9999     2 (distinct)
+#            :50002    18 (Electrum Bitcoin SSL)
 
 import datetime
 from collections import Counter
