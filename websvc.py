f7db80491266055db7867cf7e992c585f79a0e18
Toralf Förster <toralf.foerster@gmx.de>
2016-08-27 19:59:26 +0200

    initital commit
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

---
+++ websvc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+#
+
+import socket
+from BaseHTTPServer import HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+ 
+class MyHandler(SimpleHTTPRequestHandler):
+  def do_GET(self):
+    return SimpleHTTPRequestHandler.do_GET(self)
+ 
+class HTTPServerV6(HTTPServer):
+  address_family = socket.AF_INET6
+ 
+def main():
+  server = HTTPServerV6(('::', 8080), MyHandler)
+  #server = HTTPServer(('127.0.0.1', 8080), MyHandler)
+  server.serve_forever()
+ 
+if __name__ == '__main__':
+  main()
---
+++ websvc.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+#set -x
+
+if [[ "$(whoami)" != "root" ]]; then
+  echo "you must be root "
+  exit 1
+fi
+
+dir=/tmp/websvc
+log=/tmp/websvc.log
+
+touch $log  || exit 1
+mkdir $dir 2>/dev/null
+echo "$(dd if=/dev/urandom 2>/dev/null | base64 | sed -e 's/[^abcdefghijklmnopqrstuvwxyz234567]//g' | cut -c1-16 | head -n1).onion" > $dir/address.txt
+cp /home/tfoerste/torutils/websvc.py /tmp
+
+chown -R websvc:websvc /tmp/websvc*
+chmod 700 /tmp/websvc.py
+chmod 640 $log
+chmod 750 $dir
+
+cd $dir
+su websvc -c "cd /tmp/websvc && /tmp/websvc.py &>> $log" &
+sleep 2
+rm /tmp/websvc.py
