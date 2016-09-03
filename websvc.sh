16bfa7c9f06e7b0e6b1d16d56772302273979520
Toralf Förster <toralf.foerster@gmx.de>
2016-09-03 19:08:29 +0200

    websvc.sh: use one hard coded path less
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- websvc.sh
+++ websvc.sh
@@ -10,10 +10,9 @@ fi
 dir=/tmp/websvc
 log=/tmp/websvc.log
 
-touch $log  || exit 1
 mkdir $dir 2>/dev/null
 echo "$(dd if=/dev/urandom 2>/dev/null | base64 | sed -e 's/[^abcdefghijklmnopqrstuvwxyz234567]//g' | cut -c1-16 | head -n1).onion" > $dir/address.txt
-cp /home/tfoerste/torutils/websvc.py /tmp
+cp $(dirname $0)/websvc.py /tmp
 
 chown -R websvc:websvc /tmp/websvc*
 chmod 700 /tmp/websvc.py
@@ -24,4 +23,3 @@ cd $dir
 su websvc -c "cd /tmp/websvc && /tmp/websvc.py &>> $log" &
 sleep 2
 rm /tmp/websvc.py
-
