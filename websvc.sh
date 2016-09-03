d3f765432a264b8849df331777286c7bf9c02271
Toralf Förster <toralf.foerster@gmx.de>
2016-09-03 19:13:19 +0200

    websvc.sh: simplify chown logic, create log file before
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- websvc.sh
+++ websvc.sh
@@ -10,14 +10,13 @@ fi
 dir=/tmp/websvc
 log=/tmp/websvc.log
 
+touch $log
 mkdir $dir 2>/dev/null
 echo "$(dd if=/dev/urandom 2>/dev/null | base64 | sed -e 's/[^abcdefghijklmnopqrstuvwxyz234567]//g' | cut -c1-16 | head -n1).onion" > $dir/address.txt
 cp $(dirname $0)/websvc.py /tmp
 
 chown -R websvc:websvc /tmp/websvc*
-chmod 700 /tmp/websvc.py
-chmod 640 $log
-chmod 750 $dir
+chmod go-rwx /tmp/websvc*
 
 cd $dir
 su websvc -c "cd /tmp/websvc && /tmp/websvc.py &>> $log" &
