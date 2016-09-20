7fbc95e19e8fd9f86e6c12db177e182cce42dc64
Toralf Förster <toralf.foerster@gmx.de>
2016-09-20 15:42:32 +0200

    unlock_tor.sh: encrypt just the data subdir
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- unlock_tor.sh
+++ unlock_tor.sh
@@ -2,12 +2,19 @@
 #
 #set -x
 
-#	unlock an ext4-fs encrypted directory and start the Tor daemon
+# unlock an ext4-fs encrypted directory and start the Tor daemon
 #
 
-user="tfoerste"
-host="mr-fox"
-ldir=~/hetzner/$host
+ldir=~/hetzner/$host  # local dir
+
+host="mr-fox"         # Tor relay
+user="tfoerste"       # remote user
+
+# preparation:
+#
+# 1.  echo "0x$(head -c 16 /dev/urandom | xxd -p)" > $ldir/.cryptoSalt
+# 2.  pwgen -s 32 -1 | head -n1 > $ldir/.cryptoPass
+# 3.  create an empty /var/lib/tor/data at the Tor relay, owned by the tor user
 
 if [[ ! -f $ldir/.cryptoSalt || ! -f $ldir/.cryptoPass ]]; then
   echo "files not found"
@@ -15,7 +22,7 @@ if [[ ! -f $ldir/.cryptoSalt || ! -f $ldir/.cryptoPass ]]; then
 fi
 
 scp $ldir/.cryptoSalt $host:/tmp
-cat $ldir/.cryptoPass | ssh $user@$host 'sudo -u tor e4crypt add_key -S $(cat /tmp/.cryptoSalt) /var/lib/tor && sudo /etc/init.d/tor start; rm /tmp/.cryptoSalt'
+cat $ldir/.cryptoPass | ssh $user@$host 'sudo -u tor e4crypt add_key -S $(cat /tmp/.cryptoSalt) /var/lib/tor/data && sudo /etc/init.d/tor start; rm /tmp/.cryptoSalt'
 
 exit $?
 
