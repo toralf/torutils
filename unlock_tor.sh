eb547eff8c463f5d5a3f9984263c4fedc5f1bfe2
Toralf Förster <toralf.foerster@gmx.de>
2016-09-20 16:15:44 +0200

    unlock_tor.sh: comments
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- unlock_tor.sh
+++ unlock_tor.sh
@@ -5,23 +5,29 @@
 # unlock an ext4-fs encrypted directory and start the Tor daemon
 #
 
+# one time preparation before 1st run of this script:
+#
+# 1.  echo "0x$(head -c 16 /dev/urandom | xxd -p)" > $ldir/.cryptoSalt
+# 2.  pwgen -s 32 -1 | head -n1 > $ldir/.cryptoPass
+# 3.  ensure that /var/lib/tor/data is empty at the Tor relay and owned by the tor user
+
 ldir=~/hetzner/$host  # local dir
 
 host="mr-fox"         # Tor relay
 user="tfoerste"       # remote user
-
-# preparation:
-#
-# 1.  echo "0x$(head -c 16 /dev/urandom | xxd -p)" > $ldir/.cryptoSalt
-# 2.  pwgen -s 32 -1 | head -n1 > $ldir/.cryptoPass
-# 3.  create an empty /var/lib/tor/data at the Tor relay, owned by the tor user
 
 if [[ ! -f $ldir/.cryptoSalt || ! -f $ldir/.cryptoPass ]]; then
   echo "files not found"
   exit 1
 fi
 
+# copy the salt to the Tor relay to a tmpfs location
+# the password will never leave the local system
+#
 scp $ldir/.cryptoSalt $host:/tmp
+
+# de-crypt the data directory
+#
 cat $ldir/.cryptoPass | ssh $user@$host 'sudo -u tor e4crypt add_key -S $(cat /tmp/.cryptoSalt) /var/lib/tor/data && sudo /etc/init.d/tor start; rm /tmp/.cryptoSalt'
 
 exit $?
