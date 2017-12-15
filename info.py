cf05c0c057ff3355315e1877f34e1294d14c20e8
Toralf Förster <toralf.foerster@gmx.de>
2017-12-15 23:59:10 +0100

    info.py: fix non-relay classification
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- info.py
+++ info.py
@@ -145,11 +145,11 @@ def main():
         inc_ports_ext ('=> exit')
 
       else:
-        if not conn.is_ipv6 and lport == ORPort or conn.is_ipv6 and lport == ORPort6:
+        if not (conn.is_ipv6 and lport == ORPort or conn.is_ipv6 and lport == ORPort6):
           inc_ports_int('ORPort   <= outer')
-        elif not conn.is_ipv6 and lport == DirPort or conn.is_ipv6 and lport == DirPort6:
+        elif not (conn.is_ipv6 and lport == DirPort or conn.is_ipv6 and lport == DirPort6):
           inc_ports_int('DirPort  <= outer')
-        elif (lport == ControlPort):
+        elif lport == ControlPort:
           inc_ports_int('CtrlPort <= local')
         else:
           inc_ports_ext ('=> non exit port')
