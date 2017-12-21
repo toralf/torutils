f2051debd1c23af4a5cf0cc32b23380cc7a53f21
Toralf Förster <toralf.foerster@gmx.de>
2017-12-21 15:05:34 +0100

    fuzz.sh: fix last commit
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- fuzz.sh
+++ fuzz.sh
@@ -81,10 +81,10 @@ function checkResult()  {
     pid=$d/fuzz.pid
     if [[ -s $pid ]]; then
       kill -0 $(cat $pid) 2>/dev/null
-      echo "$d finished"
       if [[ $? -ne 0 ]]; then
+        echo "$d finished"
         if [[ -f "$(ls $d/*.tbz2 2>/dev/null)" ]]; then
-          echo "has findings"
+          echo "$d has findings"
           mv $d ~/findings
         else
           mv $d ~/done
