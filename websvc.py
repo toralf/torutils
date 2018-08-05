5849b29560046691debb8e43f76e7ea323fe79ae
Toralf Förster <toralf.foerster@gmx.de>
2018-08-05 14:47:02 +0200

    websvc.py: add stdout, remove port randomness
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- websvc.py
+++ websvc.py
@@ -5,7 +5,6 @@ import socket
 import argparse
 import re
 import sys
-import random
 
 if sys.version_info[0] == 3:
   from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -17,10 +16,15 @@ else:
 class HTTPServerV6(HTTPServer):
   address_family = socket.AF_INET6
 
+class MyHandler(SimpleHTTPRequestHandler):
+  def do_GET(self):
+    print(self.path)
+    return SimpleHTTPRequestHandler.do_GET(self)
+
 def main():
   is_ipv6 = False
   address = 'localhost'
-  port = random.randint (1025,65535)
+  port = 1234
 
   parser = argparse.ArgumentParser()
   parser.add_argument("--address", help="default: " + address)
@@ -44,9 +48,9 @@ def main():
         is_ipv6 = True
 
   if is_ipv6:
-    server = HTTPServerV6((address, port), SimpleHTTPRequestHandler)
+    server = HTTPServerV6((address, port), MyHandler)
   else:
-    server = HTTPServer((address, port), SimpleHTTPRequestHandler)
+    server = HTTPServer((address, port), MyHandler)
 
   server.serve_forever()
 
