5deadd8e417dd2ca27f77a8044a205165567680a
Toralf Förster <toralf.foerster@gmx.de>
2019-05-11 11:08:44 +0200

    key-expires.py: initial commit
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

---
+++ key-expires.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import codecs
+import sys
+import time
+
+with open('/var/lib/' + sys.argv[1] + '/data/keys/ed25519_signing_cert', 'rb') as f:
+    cert = f.read()
+    expire = int(codecs.encode(cert[35:38], 'hex'), 16) * 3600
+    now = time.time()
+    print(int(expire-now))
