e58c0d6f477dec0ad3e1136da86b50df635c3439
Toralf Förster <toralf.foerster@gmx.de>
2019-05-25 23:04:51 +0200

    run.sh: refactored
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- run.sh
+++ run.sh
@@ -4,21 +4,17 @@
 
 # start/stop fuzzers to achieve $1 running instances
 
-/opt/torutils/fuzz.sh -c -a
-
 if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
-jobs=${1:-0}
-let "n = $jobs - $(pgrep -c afl-fuzz)"
+$(dirname $0)/fuzz.sh -c -a
 
-if    [[ $n -gt 0 ]]; then
-  /opt/torutils/fuzz.sh -u -s $n
+jobs=$1
+let "n = $jobs - $(pgrep -ac afl-fuzz)"
 
-elif  [[ $n -ne 0 ]]; then
-  n=$(echo "$n" | sed -e 's/-//g')
-  kill -15 $(pgrep afl-fuzz | xargs -n 1 | shuf -n $n)
-  sleep 2
-  /opt/torutils/fuzz.sh -a
+if   [[ $n -gt 0 ]]; then
+  $(dirname $0)/fuzz.sh -u -s $n
+elif [[ $n -lt 0 ]]; then
+  kill -15 $(pgrep afl-fuzz | xargs -n 1 | shuf | head $n)
 fi
