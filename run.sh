a35591910ec9fba510a3bb20858b6b46669039bd
Toralf Förster <toralf.foerster@gmx.de>
2019-07-08 19:59:42 +0200

    run.sh: fix if n < # of running instances
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- run.sh
+++ run.sh
@@ -16,5 +16,7 @@ let "n = $jobs - $(pgrep -ac afl-fuzz)"
 if   [[ $n -gt 0 ]]; then
   $(dirname $0)/fuzz.sh -u -s $n
 elif [[ $n -lt 0 ]]; then
-  kill -15 $(pgrep afl-fuzz | xargs -n 1 | shuf | head $n)
+  kill -15 $(pgrep afl-fuzz | xargs -n 1 | shuf | head $n)  # $n is negative
 fi
+
+$(dirname $0)/fuzz.sh -c -a
