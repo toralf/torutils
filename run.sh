25d54363d5ba96720f76b73e033eab8570b64c40
Toralf Förster <toralf.foerster@gmx.de>
2020-05-28 17:02:23 +0200

    run.sh; polishing
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- run.sh
+++ run.sh
@@ -2,23 +2,30 @@
 #
 # set -x
 
-# restart or stop fuzzers to have $1 running instances
+
+# start or stop fuzzers to have $1 runnning instances
+
 
 if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
+desired=$1
+
+# wrap up last hour
 $(dirname $0)/fuzz.sh -c -a
 
-jobs=$1
-let "n = $jobs - $(pgrep -ac afl-fuzz)"
+pids=$(pgrep -f '/usr/bin/afl-fuzz -i /home/torproject/tor-fuzz-corpora/')
+let "diff = $desired - $(echo $pids | wc -w)"
 
-if   [[ $n -gt 0 ]]; then
-  # update before starting a new instance
-  $(dirname $0)/fuzz.sh -u -s $n
-elif [[ $n -lt 0 ]]; then
-  # kill $n arbitrarily choosen instances
-  kill -15 $(pgrep "afl-fuzz" | xargs -n 1 | shuf -n ${n##*-})
+if   [[ $diff -gt 0 ]]; then
+  $(dirname $0)/fuzz.sh -u -s $diff
+
+elif [[ $diff -lt 0 ]]; then
+  victims=$(echo $pids | xargs -n 1 | shuf -n ${diff##*-})
+  if [[ -n "$victims" ]]; then
+    kill -15 $victims
+    sleep 5
+    $(dirname $0)/fuzz.sh -c -a
+  fi
 fi
-
-$(dirname $0)/fuzz.sh -c -a
