5402ea38f0de5d66c05f67cde26b38eacce3dc28
Toralf Förster <toralf.foerster@gmx.de>
2020-06-09 21:33:15 +0200

    run.sh: cd into script dir
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- run.sh
+++ run.sh
@@ -3,29 +3,31 @@
 # set -x
 
 
-# start or stop fuzzers to have $1 runnning instances
+# start or stop fuzzer(s) to achieve $1 runnning instances
 
+
+set -euf
+cd $(dirname $0)
 
 if [[ $# -ne 1 ]]; then
   exit 1
 fi
-
 desired=$1
 
-# wrap up last hour
-$(dirname $0)/fuzz.sh -c -a
+./fuzz.sh -c -a
 
 pids=$(pgrep -f '/usr/bin/afl-fuzz -i /home/torproject/tor-fuzz-corpora/')
+
 let "diff = $desired - $(echo $pids | wc -w)"
 
 if   [[ $diff -gt 0 ]]; then
-  $(dirname $0)/fuzz.sh -u -s $diff
+  ./fuzz.sh -u -s $diff
 
 elif [[ $diff -lt 0 ]]; then
   victims=$(echo $pids | xargs -n 1 | shuf -n ${diff##*-})
   if [[ -n "$victims" ]]; then
     kill -15 $victims
     sleep 5
-    $(dirname $0)/fuzz.sh -c -a
+    ./fuzz.sh -c -a
   fi
 fi
