2fb2f4b1e127ade081181c521a57f1082c95c3d0
Toralf Förster <toralf.foerster@gmx.de>
2020-07-01 15:47:25 +0200

    bin/bwrap.sh: refactor cgset logic
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- fuzz_helper.sh
+++ fuzz_helper.sh
@@ -7,26 +7,25 @@
 
 
 function CgroupCreate() {
-  name=${1##*/}
-  pid=$2
+  local name=/local/fuzzer_${1##*/}
+  local pid=$2
 
-  if [[ -z "$name" || -n "${pid//[0-9]}" ]]; then
-    exit 1
-  fi
+  cgcreate -g memory:$name -g cpu:$name
+  cgset -r memory.use_hierarchy=1           $name
+  cgset -r memory.limit_in_bytes=30G        $name
+  cgset -r memory.memsw.limit_in_bytes=40G  $name
+  echo "$pid" > /sys/fs/cgroup/memory/$name/tasks
 
-  cgcreate -g memory:/local/fuzzer_${name}
-  cgset -r memory.use_hierarchy=1 -r memory.limit_in_bytes=30G -r memory.memsw.limit_in_bytes=40G -r memory.tasks=$$ fuzzer_${name}
-
-  cgcreate -g cpu:/local/fuzzer_${name}
-  cgset -r cpu.cfs_quota_us=150000 -r cpu.cfs_period_us=100000 -r cpu.tasks=$$ fuzzer_${name}
+  cgset -r cpu.cfs_quota_us=150000  $name
+  cgset -r cpu.cfs_period_us=100000 $name
+  echo "$pid" > /sys/fs/cgroup/cpu/$name/tasks
 }
 
 
 function CgroupDelete()  {
-  name=${1##*/}
+  local name=/local/fuzzer_${1##*/}
 
-  cgdelete -g cpu:/local/fuzzer_${name}
-  cgdelete -g memory:/local/fuzzer_${name}
+  cgdelete -g memory:$name -g cpu:$name
 }
 
 
@@ -41,7 +40,14 @@ if [[ "$(whoami)" != "root" ]]; then
   exit 1
 fi
 
+if [[ -z "$1" ]]; then
+  exit 1
+fi
+
 if [[ $# -eq 2 ]]; then
+  if [[ "${2//[0-9]}" ]]; then
+    exit 1
+  fi
   CgroupCreate $1 $2
 else
   CgroupDelete $1
