a4c8e07ddf6a195e1e2a77cdb89ba325e64aeb92
Toralf Förster <toralf.foerster@gmx.de>
2020-10-31 20:51:41 +0100

    orstatus.py: improve speed
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- orstatus.py
+++ orstatus.py
@@ -25,7 +25,8 @@ def main():
     controller.add_event_listener(orconn_listener, EventType.ORCONN)
 
     for desc in parse_file('/var/lib/tor/data/cached-consensus'):
-      desc_versions[desc.fingerprint] = desc.version
+      ip = 'v4' if is_valid_ipv4_address(desc.address) else 'v6'
+      desc_versions[desc.fingerprint] = [desc.address, desc.or_port, ip, desc.version]
 
     while True:
       try:
@@ -37,17 +38,13 @@ def main():
 async def orconn_event(controller, event):
   if event.status == ORStatus.CLOSED:
     fingerprint = event.endpoint_fingerprint
-    desc = await controller.get_network_status(fingerprint, None)
-
-    print ('%-12s %s' % (event.reason, fingerprint), end='')
-    if (desc):
-      ip = 'v4' if is_valid_ipv4_address(desc.address) else 'v6'
-      version = desc.version
-      if not version:
-        version = desc_versions[fingerprint] if fingerprint in desc_versions else 'n/a'
-      print(' %15s %5i %s %s' % (desc.address, desc.or_port, ip, version))
-    else:
-      print('')
+    if fingerprint:
+      print ('%-12s %s' % (event.reason, fingerprint), end='')
+      if fingerprint in desc_versions:
+        address, or_port, ip, version = desc_versions[fingerprint]
+        print(' %15s %5i %s %s' % (address, or_port, ip, version))
+      else:
+        print('')
 
 
 if __name__ == '__main__':
