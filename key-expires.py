4329674792f62a65d9c8bf0639489a7a23d05975
Toralf Förster <toralf.foerster@gmx.de>
2021-09-18 19:39:19 +0200

    key-expires.py: spelling + comments

--- key-expires.py
+++ key-expires.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# put out the time (in seconds) before the key expires
+# put out the time (in seconds) before the medium-term signing Tor certs expires
+
 # appropriate cron job:
-# @daily    let n="$(/opt/torutils/key-expires.py /var/lib/tor/data/keys/ed25519_signing_cert) / 86400"; [[ $n -lt 35 ]] && echo "Tor key is expiring in less than $n day(s)"
+# @daily    let n="$(key-expires.py /var/lib/tor/data/keys/ed25519_signing_cert) / 86400"; [[ $n -lt 35 ]] && echo "Tor cert expires in less than $n day(s)"
 
 import codecs
 import sys
 import time
 
-# eg.: /var/lib/tor/data2/keys/ed25519_signing_cert
-#
 with open(sys.argv[1], 'rb') as f:
     cert = f.read()
-    expire = int(codecs.encode(cert[35:38], 'hex'), 16) * 3600
-    now = time.time()
-    print(int(expire-now))
+    expire = int(codecs.encode(cert[35:38], 'hex'), 16)   # expiration timestamp in hours
+    now = int(time.time())                                # current timestamp in seconds
+    print(3600*expire-now)
