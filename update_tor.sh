99da229ad461887d646ffe7f93679d2b2243e0ae
Toralf Förster <toralf.foerster@gmx.de>
2022-05-21 23:27:09 +0200

    fix 43481a52b3ac

--- update_tor.sh
+++ update_tor.sh
@@ -10,7 +10,8 @@ v=$(tor --version | head -n 1 | cut -f2 -d'(' | tr -d '(git\-).')
 h=$(su - torproject bash -c 'cd ~torproject/sources/tor; git pull &>/dev/null; git describe HEAD | sed "s,^.*-g,,g"')
 
 if [[ -n $v && -n $h && ! $v =~ $h ]]; then
+  echo " $0: $v -> $h"
   emerge -1 net-vpn/tor
-  rc-service tor  restart
-  rc-service tor2 restart
+  /sbin/rc-service tor  restart
+  /sbin/rc-service tor2 restart
 fi
