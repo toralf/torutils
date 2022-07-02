6ade5bf9a655c026d01135a258ca12c83241519c
Toralf Förster <toralf.foerster@gmx.de>
2022-07-02 19:31:39 +0200

    add git logs

--- update_tor.sh
+++ update_tor.sh
@@ -10,8 +10,13 @@ v=$(tor --version | head -n 1 | cut -f2 -d'(' | tr -d '(git\-).')
 h=$(su - torproject bash -c 'cd ~torproject/sources/tor; git pull &>/dev/null; git describe HEAD | sed "s,^.*-g,,g"')
 
 if [[ -n $v && -n $h && ! $v =~ $h ]]; then
-  echo " $0: $v -> $h"
+  cd ~torproject/sources/tor
+  export PAGER=cat
+  git log --oneline $v..$h
+  git diff --stat $v..$h
   emerge -1 net-vpn/tor
+  echo -e "\nrestart Tor\n"
   /sbin/rc-service tor  restart
+  echo -e "\nrestart Tor2\n"
   /sbin/rc-service tor2 restart
 fi
