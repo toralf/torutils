aa4e63806ffde5d3b76adfc377b8c4ea316c5687
Toralf Förster <toralf.foerster@gmx.de>
2022-07-03 15:05:51 +0200

    add hints for firewall and remove magic code

--- ddos-inbound.sh
+++ ddos-inbound.sh
@@ -3,19 +3,30 @@
 
 # catch addresses DDoS'ing the OR port
 # https://gitlab.torproject.org/tpo/core/tor/-/issues/40636
+# https://gitlab.torproject.org/tpo/core/tor/-/issues/40637
+
+
+# quick check:
+#
+# ddos-inbound.sh | grep "^r"
+#
+# feed the firewall:
+#
+# ddos-inbound.sh | grep ^address | awk '{ print $2 }' | sort -u | while read s; do iptables -I INPUT -p tcp --source $s -j DROP; done
+
 
 set -euf
 export LANG=C.utf8
 
 limit=${1:-50}
-relays=${2:-"65.21.94.13:443 65.21.94.13:9001"}
+relays=${2:-$(grep "^ORPort" /etc/tor/torrc{,2} | awk '{ print $2 }' | grep -v -F ']')}
 
 echo -e "limit $limit"
 
 for relay in $relays
 do
+  echo
   read -r ip orport < <(tr ':' ' ' <<< $relay)
-  echo -e "\nrelay $relay"
   ss --tcp -n |\
   grep "^ESTAB" |\
   grep " $relay " |\
@@ -37,8 +48,9 @@ do
           print "address $ip $h{$ip}\n";
         }
       }
-      print "or:'$orport' $ips $conns\n";
+      print "relay:'$relay' $ips $conns\n";
     }
   }' |\
   column -t
+  echo
 done
