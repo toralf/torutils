48b703e5080b0e6368a99db6e507af271967e15b
Toralf Förster <toralf.foerster@gmx.de>
2022-07-21 22:33:04 +0200

    show the commit id range

--- update_tor.sh
+++ update_tor.sh
@@ -9,12 +9,12 @@ set -euf
 export LANG=C.utf8
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 
-
 v=$(tor --version | head -n 1 | cut -f2 -d'(' | tr -d '(git\-).')
 h=$(su - torproject bash -c 'cd ~torproject/sources/tor; git pull &>/dev/null; git describe HEAD | sed "s,^.*-g,,g"')
 
 if [[ -n $v && -n $h && ! $v =~ $h ]]; then
   date
+  echo -e "update $v..$h\n"
   cd ~torproject/sources/tor
   export PAGER=cat
   git log --oneline $v..$h
