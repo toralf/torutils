3a7e7ae825b4e7e97d5981c0e75dfc415e439e23
Toralf Förster <toralf.foerster@gmx.de>
2022-09-03 14:35:35 +0200

    use an own git repo

--- update_tor.sh
+++ update_tor.sh
@@ -2,7 +2,9 @@
 # set -x
 
 
-# update tor to latest -git
+# this works under Gentoo Linux only and and only
+# if net-vpn/tor-9999 is unmasked (https://github.com/toralf/tgro/blob/main/net-vpn/tor/)
+# but then: keep Tor service(s) at -git HEAD
 
 
 #######################################################################
@@ -10,13 +12,20 @@ set -euf
 export LANG=C.utf8
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 
-v=$(tor --version | head -n 1 | cut -f2 -d'(' | tr -d '(git\-).')
-h=$(su - torproject bash -c 'cd ~torproject/sources/tor; git pull &>/dev/null; git describe HEAD | sed "s,^.*-g,,g"')
+cd ~
+if [[ ! -d ~/tor ]]; then
+  git clone https://git.torproject.org/tor.git
+fi
+cd ~/tor
+git pull 1>/dev/null
 
-if [[ -n $v && -n $h && ! $v =~ $h ]]; then
+v=$(tor --version | head -n 1 | cut -f2 -d'(' | tr -d '(git\-).')
+h=$(git describe HEAD | sed "s,^.*-g,,g")
+
+if [[ -z $v || -n $h && ! $v =~ $h ]]; then
   date
   echo -e "update $v..$h\n"
-  cd ~torproject/sources/tor
+  unset GIT_PAGER
   export PAGER=cat
   git log --oneline $v..$h
   git diff --stat $v..$h
