082a8e42bf1a2a01a80ffd940eb0ea06b887468d
Toralf Förster <toralf.foerster@gmx.de>
2022-09-20 19:53:55 +0200

    do not grep torrc, get options explicitely

--- ddos-inbound.sh
+++ ddos-inbound.sh
@@ -5,24 +5,31 @@
 # count inbound to local ORPort per remote ip address
 
 function show() {
+  local relay=$1
+
+  local v=""
+  if [[ $orip =~ ':' ]]; then
+    v="6"
+  fi
   local sum=0
   local ips=0
 
   while read -r conns ip
   do
     if [[ $conns -gt $limit ]]; then
-      printf "%-10s %-40s %5i\n" address$v $ip $conns
+      printf "%-10s %-40s %5i\n" ip$v $ip $conns
       (( ++ips ))
       (( sum = sum + conns ))
     fi
   done < <(
     ss --no-header --tcp -${v:-4} --numeric |
-    grep "^ESTAB .* $(sed -e 's,\[,\\[,g' -e 's,\],\\],g' <<< $relay) " |
+    grep "^ESTAB" |
+    grep -F " $relay " |
     awk '{ print $5 }' | sort | sed 's,:[[:digit:]]*$,,g' | uniq -c
   )
 
   if [[ $ips -gt 0 ]]; then
-    printf "relay:%-40s  adresses:%-5i  conns:%-5i\n\n" $relay $ips $sum
+    printf "relay:%-42s           ips:%-5i conns:%-5i\n\n" $relay $ips $sum
   fi
 }
 
@@ -34,31 +41,23 @@ export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 
 limit=2
 
-# is the local relay address contained in the ORPort config value ?
-relays=$(grep "^ORPort" /etc/tor/torrc{,2} 2>/dev/null | awk '{ print $2 }' | sort)
-if [[ ! $relays =~ '.' ]]; then
-  # no, so get it here
-  address=$(grep "^Address" /etc/tor/torrc | awk '{ print $2 }' | sort -u)
-  relays="$address:$relays"
-fi
+orips="65.21.94.13  [2a01:4f9:3b:468e::13]"
+orports="9001 443"
 
-while getopts l:r: opt
+while getopts i:l:p: opt
 do
   case $opt in
+    i)  orips=$OPTARG ;;
     l)  limit=$OPTARG ;;
-    r)  relays=$OPTARG ;;
+    p)  orports=$OPTARG ;;
     *)  echo "unknown parameter '$opt'"; exit 1 ;;
   esac
 done
 
-for v in '' 6
+for orip in $orips
 do
-  for relay in $relays
+  for orport in $orports
   do
-    if [[ $relay =~ '.' && "$v" = "" ]]; then
-      show
-    elif [[ ! $relay =~ '.' && "$v" = "6" ]]; then
-      show
-    fi
+    show $orip:$orport
   done
 done
