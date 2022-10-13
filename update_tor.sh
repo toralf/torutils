28a50c7220bb260688e6c2aaa6ead9f2590b7bce
Toralf Förster <toralf.foerster@gmx.de>
2022-10-13 11:48:17 +0200

    handle tagged tor version string

--- update_tor.sh
+++ update_tor.sh
@@ -18,22 +18,29 @@ if [[ ! -d ~/tor ]]; then
   git clone https://git.torproject.org/tor.git
 fi
 cd ~/tor
-git pull 1>/dev/null
 
-v=$(tor --version | head -n 1 | cut -f2 -d'(' | tr -d '(git\-).')
-h=$(git describe HEAD | sed "s,^.*-g,,g")
-
-if [[ -z $v || -n $h && ! $v =~ $h ]]; then
-  date
-  echo -e "update $v..$h\n"
-  unset GIT_PAGER
-  export PAGER=cat
-  git log --oneline $v..$h
-  git diff --stat $v..$h
-  emerge -1 net-vpn/tor
-  echo -e "\nrestart Tor\n"
-  /sbin/rc-service tor  restart
-  echo -e "\nrestart Tor2\n"
-  /sbin/rc-service tor2 restart
-  date
+if git pull 2>&1 | grep -q 'Already up to date.'; then
+  exit 0
 fi
+
+version=$(tor --version | head -n 1)
+if grep -q 'git' <<< $version; then
+  version=$(cut -f2 -d'(' <<< $version | tr -d '(git\-).')
+fi
+head=$(git describe HEAD | sed "s,^.*-g,,g")
+
+date
+
+echo -e "update $version..$head\n"
+unset GIT_PAGER
+export PAGER=cat
+git log --oneline $version..$head 2>/dev/null && git diff --stat $version..$head
+
+emerge -1 net-vpn/tor
+echo -e "\nrestart Tor\n"
+/sbin/rc-service tor  restart
+echo -e "\nrestart Tor2\n"
+/sbin/rc-service tor2 restart
+
+date
+
