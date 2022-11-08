4cfae6fdb768a8714c581ce9f94f40f19d0575b4
Toralf Förster <toralf.foerster@gmx.de>
2022-11-08 21:06:45 +0100

    update moria

--- ipv4-rules.sh
+++ ipv4-rules.sh
@@ -28,9 +28,9 @@ function addCommon() {
 
 
 function __fill_trustlist() {
-  # getent ahostsv4 snowflake-01.torproject.net. | awk '{ print $1 }' | sort -u | xargs
-  # curl -s 'https://onionoo.torproject.org/summary?search=flag:authority' -o - | jq -cr '.relays[].a[0]' | sort | xargs
-  echo 193.187.88.42 45.66.33.45 66.111.2.131 86.59.21.38 128.31.0.34 131.188.40.189 154.35.175.225 171.25.193.9 193.23.244.244 199.58.81.140 204.13.164.118 |
+  # getent ahostsv4 snowflake-01.torproject.net. | awk '{ print $1 }' | sort -un | xargs
+  # curl -s 'https://onionoo.torproject.org/summary?search=flag:authority' -o - | jq -cr '.relays[].a[0]' | sort -n | xargs
+  echo 193.187.88.42 45.66.33.45 66.111.2.131 86.59.21.38 128.31.0.24 131.188.40.189 154.35.175.225 171.25.193.9 193.23.244.244 199.58.81.140 204.13.164.118 |
   xargs -r -n 1 -P 20 ipset add -exist $trustlist
 }
 
