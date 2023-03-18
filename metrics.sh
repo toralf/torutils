b11898453dd058e9bada32f4e1bcce84c55c0909
Toralf Förster <toralf.foerster@gmx.de>
2023-03-18 21:36:27 +0100

    rename metric names + add histogram data

--- metrics.sh
+++ metrics.sh
@@ -4,6 +4,7 @@
 
 
 # use node_exporter's "textfile" feature to pump metrics into Prometheus
+# https://prometheus.io/docs/instrumenting/exposition_formats/
 
 
 #######################################################################
@@ -11,18 +12,15 @@ set -eu
 export LANG=C.utf8
 export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 
-
-ne_dir=${1:-/var/lib/node_exporter} # default directory under Gentoo Linux
-
-if [[ ! -d $ne_dir ]]; then
-  echo -e " exporter directory '$ne_dir' does not exist" >&2
+datadir=${1:-/var/lib/node_exporter} # default directory under Gentoo Linux
+if [[ ! -d $datadir ]]; then
+  echo -e " exporter directory '$datadir' does not exist" >&2
   exit 1
 fi
 
-
 tmpfile=$(mktemp /tmp/$(basename $0).XXXXXX)
 
-# iptables stats of "filter" table
+# iptables table stats
 var="torutils_packets"
 echo -e "# HELP $var Total number of packets\n# TYPE $var gauge" >> $tmpfile
 for v in "" 6
@@ -46,21 +44,63 @@ do
   done
 done
 
-# ipset stats
-var="torutils_ipsets"
+# ipset number of entries
+var="torutils_ipset_total"
 echo -e "# HELP $var Total number of ip addresses\n# TYPE $var gauge" >> $tmpfile
 for v in "" 6
 do
   ipset list -t | grep -e "^N" | xargs -n 6 | awk '/tor-ddos'$v'-/ { print $2, $6 }' |
-  while read -r name count
+  while read -r name size
   do
     orport=$(cut -f3 -d'-' <<< $name)
-    echo "$var{ipver=\"${v:-4}\",orport=\"$orport\"} $count" >> $tmpfile
+    echo "$var{ipver=\"${v:-4}\",orport=\"$orport\"} $size" >> $tmpfile
+  done
+done
+
+# ipset timeout histogram
+var="torutils_ipset_timeout"
+echo -e "# HELP $var A histogram of ipset timeout values\n# TYPE $var histogram" >> $tmpfile
+for v in "" 6
+do
+  ipset list -t | grep -e "^Name" | awk '/tor-ddos'$v'-/ { print $2 }' |
+  while read -r name
+  do
+    orport=$(cut -f3 -d'-' <<< $name)
+
+    ipset list -s $name | sed -e '1,8d' |
+    perl -wane '
+      BEGIN {
+        @arr = (0) x 24;  # 0-23 hour
+        $inf = 0;         # anything above
+        $sum = 0;
+      }
+      {
+        chomp();
+        my $hour = int(($F[2]-1)/3600);
+        if ($hour <= 23)  {
+          $arr[$hour]++;
+        } else {
+          $inf++;
+        }
+        $sum += $hour;
+      }
+
+      END {
+        my $N = 0;
+        for (my $i = 0; $i <= $#arr; $i++) {
+          $N += $arr[$i];
+          print "'${var}'_bucket{ipver=\"'${v:-4}'\",orport=\"'$orport'\",le=\"$i\"} $N\n";
+        }
+        my $count = $N + $inf;
+        print "'${var}'_bucket{ipver=\"'${v:-4}'\",orport=\"'$orport'\",le=\"+Inf\"} $count\n";
+        print "'${var}'_count{ipver=\"'${v:-4}'\",orport=\"'$orport'\"} $count\n";
+        print "'${var}'_sum{ipver=\"'${v:-4}'\",orport=\"'$orport'\"} $sum\n";
+      }' >> $tmpfile
   done
 done
 
 # iptables module hashlimit
-var="torutils_hashlimit"
+var="torutils_hashlimit_total"
 echo -e "# HELP $var Total number of ip addresses\n# TYPE $var gauge" >> $tmpfile
 for v in "" 6
 do
@@ -73,5 +113,5 @@ do
   done
 done
 
-mv $tmpfile $ne_dir/torutils.prom
-chmod a+r  $ne_dir/torutils.prom
+mv $tmpfile $datadir/torutils.prom
+chmod a+r  $datadir/torutils.prom
