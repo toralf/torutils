aad229c4bcb71e9fd114c3b139cf070469fd1462
Toralf Förster <toralf.foerster@gmx.de>
2023-05-15 20:44:18 +0200

    do not append the same line twice

--- check_ct.sh
+++ check_ct.sh
@@ -17,13 +17,13 @@ if [[ ! -f $tmpfile.old ]]; then
   touch $tmpfile.old
 fi
 
-conntrack -S | grep -v ' insert_failed=0 drop=0 ' | awk '{ print $1, $5, $6 }' | cut -f2- -d':' >>$tmpfile
+conntrack -S | grep -v ' insert_failed=0 drop=0 ' | awk '{ print $1, $5, $6 }' | cut -f2- -d':' >$tmpfile
 n=$(conntrack -C)
 if [[ $n -gt 50000 ]]; then
   echo "more than 50K in conntrack table: $n" >>$tmpfile
 fi
 
-if ! diff -q $tmpfile{,.old}; then
+if ! diff -q $tmpfile{,.old} &>/dev/null; then
   tail -v $tmpfile{,.old}
   cp $tmpfile $tmpfile.old
 fi
