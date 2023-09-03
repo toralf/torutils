e9021e81eed76896975c2e6f3ec21af2a7ea18b8
Toralf Förster <toralf.foerster@gmx.de>
2023-09-03 15:58:21 +0200

    heal crashed services in parallel

--- restart_service.sh
+++ restart_service.sh
@@ -4,19 +4,38 @@
 
 # restart a crashed service under Gentoo Linux (OpenRC)
 
+function healService() {
+  local service=${1?}
+
+  local msg="$0: detected crashed $service"
+  echo "$msg" >&2
+  logger "$msg"
+  sleep 30
+  rc-service -qq $s status
+  if [[ $? -eq 32 ]]; then
+    msg="$0: restart crashed $service"
+    echo "$msg" >&2
+    logger "$msg"
+    rc-service $s zap start
+  else
+    msg="$0: healed w/o our help $service"
+    echo "$msg" >&2
+    logger "$msg"
+  fi
+}
+
+#######################################################################
+#
 set -u
 export LANG=C.utf8
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 
 while :; do
   if [[ "$(runlevel)" == "N 3" ]]; then
-    for s in ssh $(find /etc/init.d -name 'tor*' -print0 | xargs -r -n 1 --null basename) unbound; do
+    for s in ssh unbound $(find /etc/init.d -name 'tor*' -print0 | xargs -r -n 1 --null basename); do
       rc-service -qq $s status
       if [[ $? -eq 32 ]]; then
-        msg="$0: restart crashed $s"
-        echo "$msg" >&2
-        logger "$msg"
-        rc-service $s zap start
+        healService $s &
       fi
     done
   fi
