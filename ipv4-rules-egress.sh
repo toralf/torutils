8dce051d66ecada13503539cf135fdb30a32255b
Toralf Förster <toralf.foerster@gmx.de>
2025-10-27 20:56:18 +0100

    increase ash limit default from 8 to 24

--- ipv4-rules-egress.sh
+++ ipv4-rules-egress.sh
@@ -7,7 +7,7 @@
 # examples:
 #
 # /opt/torutils/ipv4-rules-egress.sh
-# EGRESS_SUBNET_DROP="1.2.3.4/32 5.6.7.8" EGRESS_SUBNET_SLEW="9.10.11.12/20" /opt/torutils/ipv4-rules-egress.sh start 16
+# EGRESS_SUBNET_DROP="1.2.3.4/32 5.6.7.8" EGRESS_SUBNET_SLEW="9.10.11.12/20" /opt/torutils/ipv4-rules-egress.sh start 15
 
 #######################################################################
 set -euf
@@ -35,13 +35,13 @@ if [[ ${1-} == "start" ]]; then
   # block entirely
   for item in ${EGRESS_SUBNET_DROP-}; do
     read -r net mask <<<$(tr '/' ' ' <<<$item)
-    $ipt -A OUTPUT -p tcp --destination $net/${mask:-24} -m conntrack --ctstate NEW,RELATED -j DROP
+    $ipt -A OUTPUT -p tcp --dst $net/${mask:-24} -m conntrack --ctstate NEW,RELATED -j DROP
   done
 
   # slew ramp on phase e.g. after a reboot
   for item in ${EGRESS_SUBNET_SLEW-}; do
     read -r net mask <<<$(tr '/' ' ' <<<$item)
-    $ipt -A OUTPUT -p tcp --destination $net/${mask:-24} -m conntrack --ctstate NEW,RELATED -m hashlimit --hashlimit-name tor-egress --hashlimit-mode dstip,dstport --hashlimit-dstmask ${mask:-24} --hashlimit-above ${2:-8}/minute --hashlimit-burst 1 -j REJECT
-    $ipt -A OUTPUT -p tcp --destination $net/${mask:-24} -j ACCEPT # for stats
+    $ipt -A OUTPUT -p tcp --dst $net/${mask:-24} -m conntrack --ctstate NEW,RELATED -m hashlimit --hashlimit-name tor-egress --hashlimit-mode dstip,dstport --hashlimit-dstmask ${mask:-24} --hashlimit-above ${2:-24}/minute --hashlimit-burst 1 -j REJECT
+    $ipt -A OUTPUT -p tcp --dst $net/${mask:-24} -j ACCEPT # for stats
   done
 fi
