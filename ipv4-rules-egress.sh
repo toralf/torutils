acb11d5dffac4a6cd17840e3fa916b37ea3c2a22
Toralf Förster <toralf.foerster@gmx.de>
2025-10-27 22:35:01 +0100

    kick away DROP

--- ipv4-rules-egress.sh
+++ ipv4-rules-egress.sh
@@ -7,7 +7,7 @@
 # examples:
 #
 # /opt/torutils/ipv4-rules-egress.sh
-# EGRESS_SUBNET_DROP="1.2.3.4/32 5.6.7.8" EGRESS_SUBNET_SLEW="9.10.11.12/20" /opt/torutils/ipv4-rules-egress.sh start 15
+# EGRESS_SUBNET_SLEW="9.10.11.12/20" /opt/torutils/ipv4-rules-egress.sh start 15
 
 #######################################################################
 set -euf
@@ -32,16 +32,10 @@ if [[ ${1-} == "start" ]]; then
   # do not touch established connections
   $ipt -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
-  # block entirely
-  for item in ${EGRESS_SUBNET_DROP-}; do
-    read -r net mask <<<$(tr '/' ' ' <<<$item)
-    $ipt -A OUTPUT -p tcp --dst $net/${mask:-24} -m conntrack --ctstate NEW -j DROP
-  done
-
   # slew ramp on phase e.g. after a reboot
   for item in ${EGRESS_SUBNET_SLEW-}; do
     read -r net mask <<<$(tr '/' ' ' <<<$item)
     $ipt -A OUTPUT -p tcp --dst $net/${mask:-24} -m conntrack --ctstate NEW -m hashlimit --hashlimit-name tor-egress --hashlimit-mode dstip,dstport --hashlimit-dstmask ${mask:-24} --hashlimit-above ${2:-24}/minute --hashlimit-burst 1 -j REJECT
-    # $ipt -A OUTPUT -p tcp --dst $net/${mask:-24} -j ACCEPT # debug purpose
+    # $ipt -A OUTPUT -p tcp --dst $net/${mask:-24} -j ACCEPT # stats for debug purpose
   done
 fi
