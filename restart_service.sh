2cbc8dbcb7707bcf90105553c7f2c7fcf1045da1
Toralf Förster <toralf.foerster@gmx.de>
2026-05-18 21:45:14 +0200

    return 123 is a service was restarted

--- restart_service.sh
+++ restart_service.sh
@@ -12,7 +12,9 @@ function log() {
 }
 
 function healService() {
-  local service=${1?}
+  local service
+
+  service=${1?SERVICE NOT GIVEN}
 
   log "crashed: $service"
   sleep 30
@@ -20,6 +22,7 @@ function healService() {
   if [[ $? -eq 32 ]]; then
     log "restarting: $service"
     rc-service $service zap start
+    rc=123
   else
     log "healed w/o our help: $service"
   fi
@@ -31,11 +34,14 @@ set -u
 export LANG=C.utf8
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 
+rc=0
 if [[ "$(runlevel)" == "N 3" ]]; then
-  for s in unbound ssh steve $(find /etc/init.d -name 'tor*' -print0 | xargs -r -n 1 --null basename); do
+  for s in ${1-unbound ssh $(find /etc/init.d -name 'tor*' -print0 | xargs -r -n 1 --null basename)}; do
     rc-service -qq $s status
     if [[ $? -eq 32 ]]; then
       healService $s &
     fi
   done
 fi
+
+exit $rc
