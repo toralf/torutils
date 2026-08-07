6d4d618ac98173bc7c7d2464094fe4b497a57535
Toralf Förster <toralf.foerster@gmx.de>
2026-08-07 15:16:02 +0200

    watchdog: respect iowait only, not idle

--- watchdog.sh
+++ watchdog.sh
@@ -2,35 +2,26 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
-# restart Tor if CPU is under pressure or metrics can't be scraped
+# restart Tor if system stucks
 
 set -euf
 export LANG=C.utf8
 export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 
-type curl logger mpstat service tor >/dev/null
+type logger mpstat service >/dev/null
 
 i=0
-j=0
 while :; do
-  read -r iowait idle < <(mpstat --dec=0 -P 'ALL' 60 1 | awk '/^Average:  *all / { print $6, $12 }')
+  read -r iowait < <(mpstat --dec=0 -P 'ALL' 60 1 | awk '/^Average:  *all / { print $6 }')
 
-  if ((iowait >= 30)); then
+  if ((iowait >= 25)); then
     ((++i))
-  elif ((idle >= 20 && iowait <= 20 && i > 0)); then
+  elif ((iowait <= 15 && i > 0)); then
     ((i--))
   fi
 
-  # if grep -q "^MetricsPort 127.0.0.1:9052$" /etc/tor/torrc; then
-  #   if curl -m 3 -s localhost:9052/metrics | grep -q .; then
-  #     j=0
-  #   else
-  #     ((++j))
-  #   fi
-  # fi
-  #
-  if ((i + j > 10)); then
-    logger -s "WARNING: $(basename $0) is restarting Tor (i=$i j=$j)"
+  if ((i > 5)); then
+    logger -s "WARNING: $(basename $0) is restarting Tor"
     service tor stop
     sleep 30
     service tor start
