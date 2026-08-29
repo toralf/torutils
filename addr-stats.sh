e59aa410c0f8a1e790372c112cf65cf7136b57ba
Toralf Förster <toralf.foerster@gmx.de>
2026-08-29 12:48:52 +0200

    addr-stats.sh: call direct and not via bash function

--- addr-stats.sh
+++ addr-stats.sh
@@ -2,19 +2,23 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
-# goal: print number of unique IPvx addresses per network block of tarpit set(s)
+# goal: count unique IP addresses per network block
 
 # call:
-#   cut -f 1 -d ' ' ~/tmp/tor-relays/fw/*/torutils-tarpit-v4 | sort -u | NETMASK=16 ~/devel/torutils/addr-stats.sh | head -n 40
+#   sort -u ~/tmp/tor-relays/fw/*/torutils-tarpit-v4 | NETMASK=16 ~/devel/torutils/addr-stats.sh | head -n 40
+#   ipset list torutils-ddos-v4-443-32 | sed -e '1,8d' | cut -f 1 -d ' '| /opt/torutils/addr-stats.sh | head
 
-aggregate_ip() {
-  python3 -c "
+set -euf
+export LANG=C.utf8
+export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
+
+python3 -c "
 import signal, sys, ipaddress
 from collections import Counter
 
 signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
-user_prefix = '$prefix'
+user_prefix = '${NETMASK:-${1-}}'
 counter = Counter()
 
 for line in sys.stdin:
@@ -37,11 +41,3 @@ for line in sys.stdin:
 for net, count in counter.most_common():
     print(f'{count:7} {net}')
 "
-}
-
-set -euf
-export LANG=C.utf8
-export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
-
-prefix="${NETMASK:-${1-}}"
-aggregate_ip
